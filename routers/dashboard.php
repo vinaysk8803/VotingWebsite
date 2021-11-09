43820646aaeea55c56f74730821a970dbd02396a
Vinay <vinaysinghvsk8803@gmail.com>
2021-11-09 16:12:45 +0530

    FINAL WSBITE

--- routers/dashboard.php
+++ routers/dashboard.php
@@ -26,73 +26,153 @@ else{
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link rel="stylesheet" href="../css/dashboard.css">
+    
 </head>
 <style>
-    #Backbtn {
-        float: left;
-    }
+  .logo{
+      margin-left: 50px;
+  }
+.navbar{
+   width: 100%;
+   margin-top: 5px;
+   padding: 15px 0;
+   display: flex;
+   align-items: center;
+   justify-content: space-between;
+   
+}
 
-    #logoutbtn {
-        float: right;
-        margin: 10px;
-    }
+.navbar ul li{
+    list-style: none;
+    display: inline-block;
+    margin: 0 20px;
+    position: relative;
+}
+.navbar ul li a{
+   text-decoration: none;
+   color: black;
+   text-transform: uppercase;
 
-    #Profile {
-        background-color: white;
-        width: 30%;
-        padding: 20px;
-        float: left;
-    }
+}
+.navbar ul li::after{
+    content: '';
+    height: 3px;
+    width: 0px;
+    background: #009688;
+    position: absolute;
+    left: 0;
+    bottom: -10px;
+    transition: 0.5s;
+}
+.navbar ul li:hover::after{
+    width: 100%;
+}
+ 
+#Group {
+    background-color: #212529;
+    width: 60%;
+    padding: 20px;
+    float: right;
+}
 
-    #Group {
-        background-color: white;
-        width: 60%;
-        padding: 20px;
-        float: right;
-    }
+#votebtn {
+    cursor: pointer;
+    font-size: 15px;
+    background-color: #346eec;
+    color: white;
+    border-radius: 5px;
+}
 
-    #votebtn {
-        cursor: pointer;
-        font-size: 15px;
-        background-color: #346eec;
-        color: white;
-        border-radius: 5px;
-    }
+#voted{
+    cursor: pointer;
+    font-size: 15px;
+    background-color: green;
+    color: white;
+    border-radius: 5px;
+}
 
-    .mainpanel {
-        padding: 10px;
-    }
-    #voted{
-        cursor: pointer;
-        font-size: 15px;
-        background-color: green;
-        color: white;
-        border-radius: 5px;
-    }
+
+
+button{
+    cursor: pointer;
+    font-size: 15px;
+    background-color: #346eec;
+    color: white;
+    border-radius: 5px;
+}
+*{
+   
+    box-sizing: border-box;
+    font-family: sans-serif;
+}
+body{
+   background-image: url(../bgimages/wave.png);
+   background-size: cover;
+   background-position: center center;
+   background-repeat: no-repeat;
+   background-attachment: fixed;
+   height: 100vh;
+
+}
+.profile-card{
+    width: 500px;
+    margin: auto;
+    margin-top: 12px;
+    margin-bottom: 12px;
+    background: #ffffffd9;
+    border-radius: 10px;
+    box-shadow: 2px 4px 10px 2px rgba(0,0,0,0.5);
+    position: absolute;
+    font-weight: 800;
+    opacity: 0.98;
+    
+}
+.image-container{
+    position: relative;
+
+}
+.image-container img{
+    width: 29%;
+    border-radius: 50%;
+    margin-top: 15px;
+    margin-left: 190px;
+    height: 152px;
+
+}
+
+.main-container{
+    padding: 32px 20px 20px 20px;
+
+
+}
 </style>
 
 <body>
-    <div class="mainsection">
-        <center>
-            <a class="headersection">
-                <a href="../"><button id="Backbtn"> back</button></a>
-                <a href="../routers/logout.php"><button id="logoutbtn">Log-Out</button></a>
-                <h1>Online voting system</h1>
-                <hr>
+    <div class="banner">
+        <div class="navbar">
+            <h2 class="logo">E-Vote</h2>
+            <ul>
+                <li class="item"><a href="../homepage/home.html">Home</a></li>
+                <li class="item"><a href="logout.php">Log-Out</a></li>
+               
+            </ul>
+        </div>
+   
+        <div class="profile-card">
+            <div class="image-container">
+                <img src="../uploads/<?php  echo $userdata['photo'] ?>" height="100" width="100">
+
             </div>
-        </center>
-        <div class="mainpanel">
-            <div id="Profile">
-                <center><img src="../uploads/<?php  echo $userdata['photo'] ?>" height="100" width="100"></center>
-                <br><br>
-                <b>Name:</b>
-                <?php  echo $userdata['name']?><br><br>
-                <b>Mobile no:</b>
-                <?php  echo $userdata['mobile']?><br><br>
-                <b>Adress:</b>
-                <?php  echo $userdata['address']?><br><br>
-                <b>Status:</b>
-                <?php  echo $status ?><br><br>
+            <div class="main-container">
+                <p>Name:
+            <?php  echo $userdata['name']?></p>
+                <p> Mobile No:
+                <?php  echo $userdata['mobile']?></p>
+                <p>Address:
+                <?php  echo $userdata['address']?></p>
+                <b style="font-size: 16.5px;font-weight: bolder;">Status:</b>
+                <?php  echo $status ?>
+            </div>
             </div>
             <div id="Group">
             <?php 
@@ -101,11 +181,12 @@ else{
                  for($i=0;$i<count($groupsdata); $i++){
                     ?>
                 <div>
-                    <img style = "float:right" src="../uploads/<?php  echo $groupsdata[$i]['photo']  ?>" height="100" width="100">
-                    <b>Group Name: </b>
-                        <?php echo $groupsdata[$i]['name']        ?>
-                   <br><br>
-                    <b>Votes: </b> <?php echo $groupsdata[$i]['votes'] ?> <br><br>
+                    <img style = "float:right;border-radius: 50%;" src="../uploads/<?php  echo $groupsdata[$i]['photo']  ?>" height="100" width="100">
+                    <b style = "color:red; font-size: 20px;" >Group Name: </b>
+                       <b style = "color:white;font-size: 22px;"> <?php echo $groupsdata[$i]['name']        ?></b> <br><br>
+                   
+                    <b style="color: #198754;font-size: 22px;">Votes: </b> 
+                    <b style = "color:white;font-size: 20px;"><?php echo $groupsdata[$i]['votes'] ?> </b><br><br>
                     <form action="../api/vote.php" method = "POST">
                         <input type="hidden" name="gvotes" value="<?php echo $groupsdata[$i]['votes'] ?>">
                         <input type="hidden" name="gid" value="<?php echo $groupsdata[$i]['id'] ?>">
@@ -126,7 +207,7 @@ else{
             
                     </form>
                 </div>
-              <hr>
+              <hr style="border: 1px solid #6610f2;">
 <?php 
               
  
@@ -142,7 +223,7 @@ else{
          
             </div>
         </div>
-    </div>
+    
 
 
 </body>
